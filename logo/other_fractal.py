754665b17d5167aa16c5691e4f01e8a8553e7fad
rexapex <james@thesugdens.com>
2015-08-18 12:25:50 +0100

    Running logo/other_fractal.py

--- logo/other_fractal.py
+++ logo/other_fractal.py
@@ -8,7 +8,7 @@ def segment(scale, detail):
     move(scale)
   else:
     segment(scale / 3.0, detail - 1)
-    turn(-60)
+    turn(60)
     segment(scale / 3.0, detail - 1)
     turn(120)
     segment(scale / 3.0, detail - 1)
