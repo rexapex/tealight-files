8361b7367bcd73fd3772cb437f231e851d8839f7
rexapex <james@thesugdens.com>
2015-08-18 15:23:41 +0100

    Running robot/vine.py

--- robot/vine.py
+++ robot/vine.py
@@ -20,6 +20,11 @@ def branch(dir):
       branch(-1)
     elif right_side() == 'fruit':
       branch(1)
+    else
+      turn(2)
+      while touch() == None:
+        move()
+        
     n = n + 1
 
 def go():
