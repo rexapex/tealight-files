6f5c7aabbcd0093ecec2448c54cdc7110e446bdd
rexapex <james@thesugdens.com>
2015-08-18 12:08:53 +0100

    Running logo/chess.py

--- logo/chess.py
+++ logo/chess.py
@@ -1,7 +1,23 @@
 from tealight.logo import move, turn
 
+def square(side):
+  for i in range(0,4):
+    move(side)
+    turn(90)
+
 def chessboard():
-  
+  sqSize = 8
+  for i in range(0, 8):
+    for j in range(0, 8):
+      square(sqSize)
+      move(sqSize)
+    angle = 90
+    if(i % 2 == 1)
+      angle *= -1
+    turn(angle)
+    move(sqSize)
+    turn(angle)
     
 
+turn(-90)
 chessboard()
