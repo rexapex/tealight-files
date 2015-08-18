4e191f617ed258d7b49b8486a9a7ecc535bb0a8a
rexapex <james@thesugdens.com>
2015-08-18 15:21:37 +0100

    Running robot/vine.py

--- robot/vine.py
+++ robot/vine.py
@@ -11,13 +11,14 @@ from tealight.robot import (move,
 def branch(dir):
   turn(dir)
   
-  while True:
+  while n < 1111:
     if touch() == 'fruit':
       move()
     if left_side() == 'fruit':
       branch(-1)
     elif right_side() == 'fruit':
       branch(1)
+    n = n + 1
 
 def go():
   branch(0)
