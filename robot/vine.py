ade55638d3793c7f8b48eb9cef40130e2acbd7ec
rexapex <james@thesugdens.com>
2015-08-18 15:09:13 +0100

    Running robot/vine.py

--- robot/vine.py
+++ robot/vine.py
@@ -13,11 +13,9 @@ def branch(dir):
   run = True
   while run:
     if left_side():
-      run = False
-      branch(-1)
-    elif right_side():
-      run = False
       branch(1)
+    elif right_side():
+      branch(-1)
     else:
       move()
 
