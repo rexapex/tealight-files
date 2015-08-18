2feee91c78253bb289278095aa77def87d995d8d
rexapex <james@thesugdens.com>
2015-08-18 11:57:34 +0100

    Running logo/chess.py

--- logo/chess.py
+++ logo/chess.py
@@ -1,12 +1,16 @@
 from tealight.logo import move, turn
 
-def spiral(size):
-  
-  if size > 300:
-    return
-  
-  move(size)
-  turn(90)
-  spiral(size + 5)
-  
-spiral(0)
+def square(side):
+  for i in range(0,4):
+    move(side)
+    turn(90)
+
+def chessboard(edges, size):
+  for i in range(0, 8):
+    for j in range(0, 8):
+      square(5);
+      move(5)
+  turn(angle)
+
+turn(-90)
+chessboard(12,75)
