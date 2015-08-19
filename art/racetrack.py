fb9aad10957e86f89b97667f57174ada7e12f27a
rexapex <james@thesugdens.com>
2015-08-19 09:39:02 +0100

    Running art/racetrack.py

--- art/racetrack.py
+++ art/racetrack.py
@@ -1,3 +1,13 @@
+from tealight.art import (color, line, spot, circle, box, image, text, background)
+
+from tealight.art import screen_width, screen_height
+
+from math import sin, cos, pi
 
 
 
+def start():
+  background("track")
+  
+  
+start()
