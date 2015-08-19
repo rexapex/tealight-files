0bb382040abda116aecb5a71f4539748822ca5f8
rexapex <james@thesugdens.com>
2015-08-19 09:14:32 +0100

    Running art/images.py

--- art/images.py
+++ art/images.py
@@ -4,11 +4,11 @@ x = 0
 y = 150
 
 width = 20
-height = 8
+height = 4
 
 for i in range(0,width):
   for j in range(0,height):
-    if i == j:
+    if i%4 == j:
       image(x + i * 60, y + j * 60, "misc/YellowFlower.png")
     else:
       image(x + i * 60, y + j * 60, "misc/Clover.png")
