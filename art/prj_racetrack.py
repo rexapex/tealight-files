b5a8a8e5afdf489f0bf171faa31c17a15a922511
rexapex <james@thesugdens.com>
2015-08-20 10:25:39 +0100

    Running art/prj_racetrack.py

--- art/prj_racetrack.py
+++ art/prj_racetrack.py
@@ -1,4 +1,10 @@
+from tealight.art import (color, line, spot, circle, box, image, text, background)
 
+from tealight.art import screen_width, screen_height
+
+from math import sin, cos, pi
+
+import prj_car
 
 def start():
   car1 = car()
