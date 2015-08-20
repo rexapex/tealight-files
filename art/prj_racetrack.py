e0a8eb36e0b4a3382a5ee8dd78ef5d27b122ae23
rexapex <james@thesugdens.com>
2015-08-20 12:19:25 +0100

    Running art/prj_racetrack.py

--- art/prj_racetrack.py
+++ art/prj_racetrack.py
@@ -2,7 +2,7 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 
 from tealight.art import screen_width, screen_height
 
-from math import sin, cos, pi
+from math import sin, cos, pi, sqrt
 
 from github.rexapex.art.prj_car import car
 
@@ -41,22 +41,36 @@ def handle_frame():
   #Draw the map
   rectangle(outerWallX, outerWallY, outerWallWidth, outerWallHeight)
   box(innerWallX, innerWallY, innerWallWidth, innerWallHeight)
-  
   spot(screen_width/2, innerWallY, innerWallWidth/2)
   spot(screen_width/2, innerWallHeight+innerWallY, innerWallWidth/2)
 
   
 def testCollisions():
+  #Outer Wall Collision
   if thisCar.x <= outerWallX:
     thisCar.x = outerWallX
   elif thisCar.x >= outerWallWidth:
     thisCar.x = outerWallWidth
-  
   if thisCar.y <= outerWallY:
     thisCar.y = outerWallY
   elif thisCar.y >= outerWallHeight:
     thisCar.y = outerWallHeight
+    
+  #Inner Wall Collision
+  if boxCollision(thisCar.x, thisCar.y, innerWallX, innerWallY, innerWallWidth, innerWallHeight)
+    print "Collided with centre box"
+  
+  
+#Returns True if point is inside the box
+def boxCollision(x, y, boxX, boxY, boxWidth, boxHeight):
+  if x >= boxX and x <= boxWidth and y >= boxY and y <= boxHeight:
+    return True
+  else
+    return False
 
+  
+#Returns True if point is inside the circle
+def circleCollision():
 
 def handle_keydown(key):
   if key == "left":
