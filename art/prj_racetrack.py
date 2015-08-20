7ebf05fd6d5b396cb84d48e35389ae2a80b55c50
rexapex <james@thesugdens.com>
2015-08-20 10:33:44 +0100

    Running art/prj_racetrack.py

--- art/prj_racetrack.py
+++ art/prj_racetrack.py
@@ -6,15 +6,18 @@ from math import sin, cos, pi
 
 from github.rexapex.art.prj_car import car
 
+cars = [None]
+
 def init():
   background("track.png")
 
 def start():
   car1 = car()
   car1.draw()
+  car1[0] = car1
   
 def handle_frame():
-  
+  x=0
 
 init()
 start()
