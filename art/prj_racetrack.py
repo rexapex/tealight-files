6ca816d7b4b8899a5bd9b52606b4c07929da808f
rexapex <james@thesugdens.com>
2015-08-20 14:06:54 +0100

    Running art/prj_racetrack.py

--- art/prj_racetrack.py
+++ art/prj_racetrack.py
@@ -41,7 +41,6 @@ def handle_frame():
   #Draw the map
   color("green")
   rectangle(outerWallX, outerWallY, outerWallWidth, outerWallHeight)
-  color("blue")
   box(innerWallX, innerWallY, innerWallWidth, innerWallHeight)
   spot(screen_width/2, innerWallY, innerWallWidth/2)
   spot(screen_width/2, innerWallHeight+innerWallY, innerWallWidth/2)
