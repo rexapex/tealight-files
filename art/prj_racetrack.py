65b32956cde82859e398e0fcd895ac6dbf8ae0ee
rexapex <james@thesugdens.com>
2015-08-20 11:51:14 +0100

    Running art/prj_racetrack.py

--- art/prj_racetrack.py
+++ art/prj_racetrack.py
@@ -28,8 +28,8 @@ def handle_frame():
   
   #Draw the map
   rectangle(5, 5, screen_width-10, screen_height-10)
-  box(120, 180, screen_width-240, screen_height-180)
-  spot(screen_width/2, screen_height/4, (screen_width-240)/2)
+  box(120, 180, screen_width-240, screen_height-240)
+  #spot(screen_width/2, screen_height/4, (screen_width-240)/2)
 
 def handle_keydown(key):
   if key == "left":
