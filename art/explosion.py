c5cd3e725d09025f6f34d23aed142992a6cb0af7
rexapex <james@thesugdens.com>
2015-08-21 09:41:48 +0100

    Closing art/explosion.py

--- art/explosion.py
+++ art/explosion.py
@@ -1,1 +1,18 @@
-print "A new file!"
+
+
+
+class explosion:
+  
+  def __init__(self):
+    self.time = 50
+    self.x = 0
+    self.y = 0
+  
+  def set_pos(self, x, y)
+    self.x = x
+    self.y = y
+    
+  def update(self):
+    
+  def draw():
+    
