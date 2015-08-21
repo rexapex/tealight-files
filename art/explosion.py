138da6ffb9cb6f52d4d2b3cb02a298965925763d
rexapex <james@thesugdens.com>
2015-08-21 09:44:10 +0100

    Closing art/explosion.py

--- art/explosion.py
+++ art/explosion.py
@@ -12,11 +12,25 @@ class explosion:
     self.x = x
     self.y = y
     
-  def update(self):
+  def draw(self):
+    if self.time > 0:
+      star(self.x, self.y, "orange", 50-self.time, 50-self.time)
+      self.time -= 1
+      return False
+    if self.time == 0:
+      star(self.x, self.y, "white", 50-self.time, 50-self.time)
+      return True
+      
+  def star(self, x, y, c, size, spines):
+  
+  color(c)
+  
+  angle = 0
+  
+  for i in range(0, spines):
+    x0 = x + (size * cos(angle))
+    y0 = y + (size * sin(angle))
     
-  def draw():
-    if explosionTime > 0:
-    star(explosionX, explosionY, "orange", 50-explosionTime, 50-explosionTime)
-    explosionTime -= 1
-    if explosionTime == 0:
-      star(explosionX, explosionY, "white", 50-explosionTime, 50-explosionTime)   
+    line(x, y, x0, y0)
+    
+    angle = angle + (2 * pi / spines)
